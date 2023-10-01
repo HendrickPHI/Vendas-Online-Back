e92247ab167fe8bd4c70ce61bc091bfa3d4a9a79
HendrickPHI <91097409+HendrickPHI@users.noreply.github.com>
2023-10-01 19:34:19 -0300

    teste

--- src/user/__mocks__/createUser.mock.ts
+++ src/user/__mocks__/createUser.mock.ts
@@ -3,7 +3,7 @@ import { CreateUserDto } from '../dtos/createUser.dto';
 export const createUserMock: CreateUserDto = {
   cpf: '3214215151',
   email: 'emailMockTest@email.com',
-  name: 'qudlsjakf',
+  name: 'MOCKTEST',
   password: 'password',
   phone: '325632634',
 };
